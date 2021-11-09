f6d06b6236a0bdd7e02827ffb082b0bcbcd539d9
Rahul <officialwork.rahul@gmail.com>
2021-11-09 23:40:01 +0530

    book download

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:booknbooks/data.dart';
 import 'package:booknbooks/widgets.dart';
+import 'package:http/http.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:path_provider/path_provider.dart';
 
 
 class BookDetail extends StatefulWidget {
@@ -12,9 +16,13 @@ class BookDetail extends StatefulWidget {
 }
 
 class _BookDetailState extends State<BookDetail> {
+
+  List<Map>?data = [];
+  bool downloaded = false;
+  bool downloading = false;
+
   @override
   Widget build(BuildContext context) {
-    List<Map>?data = [];
     data = ModalRoute.of(context)!.settings.arguments as List<Map>?;
     print(data); //debug
     return Scaffold(
@@ -40,7 +48,7 @@ class _BookDetailState extends State<BookDetail> {
                 ),width: 200,height: 200,),
                 Column(
                   children: [
-                    textData(info: data[0]['book'], toBold: true, size: 24),
+                    textData(info: data![0]['book'], toBold: true, size: 24),
                     const SizedBox(height: 10,),
                     textData(info: 'Author Name', toBold: false, size: 18),
 
@@ -52,10 +60,49 @@ class _BookDetailState extends State<BookDetail> {
             textData(info: 'Description', toBold: true, size: 24),
             MaterialButton(onPressed: (){
               Navigator.pushNamed(context, '/readbook');
-            },child: textData(info:'read now', toBold: false,size: 18,))
+            },child: textData(info:'read now', toBold: false,size: 18,)),
+            MaterialButton(onPressed: () => downloadBook(),child: textData(info:!downloaded?'download':'downloaded', toBold: false,size: 18,)),
+            Visibility(child: const CircularProgressIndicator(color: Colors.white, value: null), visible: downloading,)
           ],
         ),
       ),
+    );
+  }
+
+  Future<File> getFile() async {
+    final appDocumentsDirectory = await getApplicationDocumentsDirectory();
+    String filePath = '${appDocumentsDirectory.path}/${data![0]['book']}.pdf';
+
+    return File(filePath);
+  }
+
+  Future<void> downloadBook() async {
+    // String url = data![0]['GET'];
+    downloading = true;
+    String url = 'https://sherlock-holm.es/stories/pdf/a4/1-sided/cnus.pdf';
+
+    Request request = Request('GET', Uri.parse(url));
+    StreamedResponse response = await Client().send(request);
+
+    final length = response.contentLength;
+    List<int> bytes = [];
+    File file = await getFile();
+
+    response.stream.listen(
+          (List<int> newBytes) {
+            bytes.addAll(newBytes);
+            print('${newBytes.length} added');
+      },
+      onDone: () async {
+        file.writeAsBytes(bytes);
+        print(bytes.length);
+        downloaded = true;
+        downloading = false;
+      },
+      onError: (e) {
+        print(e);
+      },
+      cancelOnError: true,
     );
   }
 }
